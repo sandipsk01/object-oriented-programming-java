504971d6803c93c849feea403ad7c8c1b81484a6
Sandip Kalekar <67133904+sandipsk01@users.noreply.github.com>
2023-01-21 12:40:44 +0530

    Update package-info.java

--- Inheritance/package-info.java
+++ Inheritance/package-info.java
@@ -1,2 +1,3 @@
 package Inheritance;
-//class can only extend one class but implement more than one
+//java doesn't support multiple inheritance but it can be achived by implements Interfaces
+//class can only extend one class but can implement more than one Interface
