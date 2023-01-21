023c05e5599d3697e91c01755eed5cf51eedef22
Sandip Kalekar <67133904+sandipsk01@users.noreply.github.com>
2023-01-21 12:02:42 +0530

    Add files via upload

---
+++ Abstract/VehicleUse.java
@@ -0,0 +1,13 @@
+package Abstract;
+
+public class VehicleUse{
+
+    public static void main(String[] args){
+        //Vehicle v= new Vehicle();   We can not create the object of abstract class and child class will also get abstract or we need get it complete in child if you want to create object of child class.
+       Vehicle v;   // we give reference of abstract class also
+//        Car c= new Car();
+//        v.print();
+//        System.out.println(v.isMotorized());
+//        v= new Bicycle();
+    }
+}
